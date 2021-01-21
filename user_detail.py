22d4e287fdce2243f32029ff635ee5294d3c7d13
Puttipong Diseno <pyybott@gmail.com>
2021-01-21 16:52:28 +0700

    add

--- user_detail.py
+++ user_detail.py
@@ -23,16 +23,18 @@ def get_holding_performance():
     result = []
 
     for i in client.get_account()["balances"]:
+        print(i["asset"] + 'USDT')
 
         pair_name_usdt= i["asset"] + 'USDT'
         chk = 0
-        try :
-            chk = float(client.get_symbol_ticker(symbol=pair_name_usdt)["price"]) * float(i["free"])
-        except:
-            chk = 0
+        if i["free"] != "0.00000000":
+            try :
+                chk = float(client.get_symbol_ticker(symbol=pair_name_usdt)["price"]) * float(i["free"])
+            except:
+                chk = 0
 
 
-        if i["free"] != "0.00000000" and i["asset"] != "USDT" and chk > 8 :
+        if i["free"] != "0.00000000" and i["asset"] != "USDT" and chk > 8 : #hide small balances
 
             print(pair_name_usdt)
             try:
@@ -44,9 +46,12 @@ def get_holding_performance():
                 for i in trades_buy:
                     my_total_dif += float(i["price"])*float(i["qty"])
                     total_qty += float(i["qty"])
+                    print(total_qty)
 
                 prof_dif = total_qty*float(trades["price"]) - my_total_dif
+                print(prof_dif)
                 percent = ((total_qty*float(trades["price"]) - my_total_dif))*100/(total_qty*float(trades["price"]))
+                print(percent)
 
                 stat = "ขาดทุน"
                 if prof_dif > 0:
